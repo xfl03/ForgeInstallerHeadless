ca94765d1284560dc48a3c1fbb3e7a5f0b3810df
Alexander Xia <xfl03@hotmail.com>
2020-02-07 07:12:24 +0800

    Add progress display

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Forge Installer 2.0 only, which is used in 1.13+.
 ```shell script
 java -cp "forge-installer-headless.jar;forge-installer.jar" me.xfl03.HeadlessInstaller -installClient PATH
 ```
-Example for using `forge-installer-headless-1.0.0` and installing `forge-1.15.2-31.1.0` to `%appdata%/.minecraft`:
+Example for using `forge-installer-headless-1.0.1` and installing `forge-1.15.2-31.1.0` to `%appdata%/.minecraft`:
 ```shell script
-java -cp "forge-installer-headless-1.0.0.jar;forge-1.15.2-31.1.0-installer.jar" me.xfl03.HeadlessInstaller -installClient %appdata%/.minecraft
+java -cp "forge-installer-headless-1.0.1.jar;forge-1.15.2-31.1.0-installer.jar" me.xfl03.HeadlessInstaller -installClient %appdata%/.minecraft
 ```
+### Progress Display
+If `-progress` parameter is added, console will show progress display only.  
+`STAGE` means which stage has reached, `START` means which task is processing.  
+After install, it will output `STAGE: INSTALL SUCCESSFUL` or `STAGE: INSTALL FAILED`.
--- src/main/java/me/xfl03/HeadlessInstaller.java
+++ src/main/java/me/xfl03/HeadlessInstaller.java
@@ -34,19 +34,29 @@ import java.util.Arrays;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
-public class HeadlessInstaller
-{
+public class HeadlessInstaller {
     public static boolean headless = false;
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
+        OptionParser parser = new OptionParser();
+        //Modified
+        OptionSpec<Void> progressOption = parser.accepts("progress", "Show progress only in console");
+        OptionSpec<File> clientInstallOption = parser.accepts("installClient", "Install a client to the specified directory").withOptionalArg().ofType(File.class).defaultsTo(new File("."));
+
+        OptionSpec<File> serverInstallOption = parser.accepts("installServer", "Install a server to the specified directory").withOptionalArg().ofType(File.class).defaultsTo(new File("."));
+        OptionSpec<File> extractOption = parser.accepts("extract", "Extract the contained jar file to the specified directory").withOptionalArg().ofType(File.class).defaultsTo(new File("."));
+        OptionSpec<Void> helpOption = parser.acceptsAll(Arrays.asList("h", "help"), "Help with this installer");
+        OptionSpec<Void> offlineOption = parser.accepts("offline", "Don't attempt any network calls");
+        OptionSet optionSet = parser.parse(args);
+
         ProgressCallback monitor;
-        try
-        {
-            monitor = ProgressCallback.withOutputs(System.out, getLog());
-        }
-        catch (FileNotFoundException e)
-        {
+        try {
+            if (optionSet.has(progressOption)) {
+                monitor = getProgressDisplayCallback(ProgressCallback.withOutputs(getLog()), System.out);
+            } else {
+                monitor = ProgressCallback.withOutputs(System.out, getLog());
+            }
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             monitor = ProgressCallback.withOutputs(System.out);
         }
@@ -63,23 +73,11 @@ public class HeadlessInstaller
         monitor.message("java.net.preferIPv4Stack=" + System.getProperty("java.net.preferIPv4Stack"));
 
         String path = HeadlessInstaller.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        if (path.contains("!/"))
-        {
+        if (path.contains("!/")) {
             monitor.stage("Due to java limitation, please do not run this jar in a folder ending with !");
             monitor.message(path);
             return;
         }
-
-        OptionParser parser = new OptionParser();
-
-        //Modified
-        OptionSpec<File> clientInstallOption = parser.accepts("installClient", "Install a client to the specified directory").withOptionalArg().ofType(File.class).defaultsTo(new File("."));
-
-        OptionSpec<File> serverInstallOption = parser.accepts("installServer", "Install a server to the specified directory").withOptionalArg().ofType(File.class).defaultsTo(new File("."));
-        OptionSpec<File> extractOption = parser.accepts("extract", "Extract the contained jar file to the specified directory").withOptionalArg().ofType(File.class).defaultsTo(new File("."));
-        OptionSpec<Void> helpOption = parser.acceptsAll(Arrays.asList("h", "help"),"Help with this installer");
-        OptionSpec<Void> offlineOption = parser.accepts("offline", "Don't attempt any network calls");
-        OptionSet optionSet = parser.parse(args);
 
         if (optionSet.has(helpOption)) {
             parser.printHelpOn(System.out);
@@ -87,14 +85,11 @@ public class HeadlessInstaller
         }
 
         int cnt = 0;
-        if (optionSet.has(offlineOption))
-        {
+        if (optionSet.has(offlineOption)) {
             DownloadUtils.OFFLINE_MODE = true;
             monitor.message("ENABELING OFFLINE MODE");
             cnt = 1;
-        }
-        else
-        {
+        } else {
             FixSSL.fixup(monitor);
         }
 
@@ -102,7 +97,7 @@ public class HeadlessInstaller
         File target = null;
 
         //Modified
-        if(optionSet.has(clientInstallOption)) {
+        if (optionSet.has(clientInstallOption)) {
             action = Actions.CLIENT;
             target = optionSet.valueOf(clientInstallOption);
         } else if (optionSet.has(serverInstallOption)) {
@@ -113,61 +108,49 @@ public class HeadlessInstaller
             target = optionSet.valueOf(extractOption);
         }
 
-        if (action != null)
-        {
-            try
-            {
+        if (action != null) {
+            try {
                 HeadlessInstaller.headless = true;
                 monitor.message("Target Directory: " + target);
                 Install install = Util.loadInstallProfile();
                 if (install.getSpec() != 0) {
-                    monitor.stage("Invalid launcher profile spec: " + install.getSpec() + " Only 0 is supported");
+                    monitor.message("Invalid launcher profile spec: " + install.getSpec() + " Only 0 is supported");
+                    monitor.stage("INSTALL FAILED");
                     System.exit(1);
                 }
-                if (!action.getAction(install, monitor).run(target, a -> true))
-                {
-                    monitor.stage("There was an error during installation");
+                if (!action.getAction(install, monitor).run(target, a -> true)) {
+                    monitor.stage("INSTALL FAILED");
                     System.exit(1);
-                }
-                else
-                {
+                } else {
                     monitor.message(action.getSuccess(install.getPath().getName()));
-                    monitor.stage("You can delete this installer file now if you wish");
+                    monitor.stage("INSTALL SUCCESSFUL");
                 }
                 System.exit(0);
-            }
-            catch (Throwable e)
-            {
+            } catch (Throwable e) {
                 monitor.stage("A problem installing was detected, install cannot continue");
                 System.exit(1);
             }
-        }
-        else if (optionSet.specs().size() > cnt)
+        } else if (optionSet.specs().size() > cnt)
             parser.printHelpOn(System.err);
         else
             launchGui(monitor);
     }
 
-    private static File getMCDir()
-    {
+    private static File getMCDir() {
         String userHomeDir = System.getProperty("user.home", ".");
         String osType = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
         String mcDir = ".minecraft";
         if (osType.contains("win") && System.getenv("APPDATA") != null)
             return new File(System.getenv("APPDATA"), mcDir);
         else if (osType.contains("mac"))
-            return new File(new File(new File(userHomeDir, "Library"),"Application Support"),"minecraft");
+            return new File(new File(new File(userHomeDir, "Library"), "Application Support"), "minecraft");
         return new File(userHomeDir, mcDir);
     }
 
-    private static void launchGui(ProgressCallback monitor)
-    {
-        try
-        {
+    private static void launchGui(ProgressCallback monitor) {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
         }
 
         Install profile = Util.loadInstallProfile();
@@ -175,32 +158,28 @@ public class HeadlessInstaller
         panel.run(monitor);
     }
 
-    private static OutputStream getLog() throws FileNotFoundException
-    {
+    private static OutputStream getLog() throws FileNotFoundException {
         File f = new File(HeadlessInstaller.class.getProtectionDomain().getCodeSource().getLocation().getFile());
         File output;
         if (f.isFile()) output = new File(f.getName() + ".log");
-        else            output = new File("installer.log");
+        else output = new File("installer.log");
 
         return new BufferedOutputStream(new FileOutputStream(output));
     }
 
-    static void hookStdOut(ProgressCallback monitor)
-    {
+    static void hookStdOut(ProgressCallback monitor) {
         final Pattern endingWhitespace = Pattern.compile("\\r?\\n$");
         final OutputStream monitorStream = new OutputStream() {
 
             @Override
-            public void write(byte[] buf, int off, int len)
-            {
+            public void write(byte[] buf, int off, int len) {
                 byte[] toWrite = new byte[len];
                 System.arraycopy(buf, off, toWrite, 0, len);
                 write(toWrite);
             }
 
             @Override
-            public void write(byte[] b)
-            {
+            public void write(byte[] b) {
                 String toWrite = new String(b);
                 toWrite = endingWhitespace.matcher(toWrite).replaceAll("");
                 if (!toWrite.isEmpty()) {
@@ -209,13 +188,47 @@ public class HeadlessInstaller
             }
 
             @Override
-            public void write(int b)
-            {
-                write(new byte[] { (byte) b });
+            public void write(int b) {
+                write(new byte[]{(byte) b});
             }
         };
 
         System.setOut(new PrintStream(monitorStream));
         System.setErr(new PrintStream(monitorStream));
+    }
+
+    static ProgressCallback getProgressDisplayCallback(ProgressCallback parent, OutputStream stdout) {
+        return new ProgressCallback() {
+            @Override
+            public void message(String message, MessagePriority priority) {
+                parent.message(message, priority);
+            }
+
+            @Override
+            public void start(String label) {
+                writeStdout("START: " + label);
+            }
+
+            @Override
+            public void stage(String label) {
+                writeStdout("STAGE: " + label);
+            }
+
+            @Override
+            public void progress(double progress) {
+                String message = String.format("%.1f", progress * 100);
+                writeStdout(message);
+            }
+
+            private void writeStdout(String message) {
+                message = message + "\n";
+                try {
+                    stdout.write(message.getBytes());
+                    stdout.flush();
+                } catch (Exception exception) {
+                    message(exception.toString(), MessagePriority.HIGH);
+                }
+            }
+        };
     }
 }
